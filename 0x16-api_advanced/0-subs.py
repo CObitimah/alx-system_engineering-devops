75f9918d1d23dc5ff0e5e0006bbc75bd8ef6d202
CObitimah <obitimahcephas@gmail.com>
2024-06-20 09:30:36 -0400

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     This function tells us how many people like a certain book (subreddit)
     """
     # The address where we ask about the book.
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
