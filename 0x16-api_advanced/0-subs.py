aabc066cc84e29c3d86da8e88674c343516fc118
CObitimah <obitimahcephas@gmail.com>
2024-06-20 12:38:14 -0400

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-0-subs module.
-"""
+"""0-subs module."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    This function tells us how many people like a certain book (subreddit)
-    """
-    # The address where we ask about the book.
+    """Query the Reddit API and returns the number of subscribers."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
