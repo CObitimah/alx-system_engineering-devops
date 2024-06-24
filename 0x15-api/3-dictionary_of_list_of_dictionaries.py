e21cab3411f4d8fbc86add04de8562a5876c936b
CObitimah <obitimahcephas@gmail.com>
2024-06-24 13:10:22 -0400

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,39 +1,32 @@
 #!/usr/bin/python3
+
+""" employee to do to csv """
+
 import json
 import requests
-
-
-def fetch_data(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def main():
-    # Fetch data from the API
-    users = fetch_data("https://jsonplaceholder.typicode.com/users")
-    todos = fetch_data("https://jsonplaceholder.typicode.com/todos")
-
-    # Create a dictionary to hold all tasks for all users
-    all_tasks = {}
-
-    for user in users:
-        user_id = user["id"]
-        username = user["username"]
-        user_tasks = [
-            {
-                "username": username,
-                "task": todo["title"],
-                "completed": todo["completed"]
-            }
-            for todo in todos if todo["userId"] == user_id
-        ]
-        all_tasks[user_id] = user_tasks
-
-    # Write the data to a JSON file
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_tasks, json_file, indent=4)
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    employee_name = ''
+    data = {}
+
+    for user in users.json():
+        user_id = user.get('id')
+        list_1 = []
+        for todo in todos.json():
+            if todo.get('userId') == user_id:
+                employee_name = user.get('username')
+                list_1.append({
+                    "username": employee_name,
+                    "task": todo.get("title"),
+                    "completed": todo.get("completed")
+                })
+
+        data.update({user.get('id'): list_1})
+
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(data, file)
