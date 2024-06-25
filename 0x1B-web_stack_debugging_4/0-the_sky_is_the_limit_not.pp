d846e037803cff8d18077561ec166cb850ae2172
CObitimah <obitimahcephas@gmail.com>
2024-06-25 08:12:27 -0400

    Sky is the limit, let us bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,18 @@
-exec { 'fix-nginx-config':
-  command => '/bin/sed -i "/worker_processes/c\worker_processes auto;" /etc/nginx/nginx.conf && \
-              /bin/sed -i "/worker_connections/c\    worker_connections 1024;" /etc/nginx/nginx.conf && \
-              /bin/sed -i "/keepalive_timeout/c\    keepalive_timeout 65;" /etc/nginx/nginx.conf && \
-              /bin/sed -i "/sendfile on;/a\    tcp_nopush on;\n    tcp_nodelay on;" /etc/nginx/nginx.conf && \
-              /bin/sed -i "/http {/a\    worker_rlimit_nofile 8192;" /etc/nginx/nginx.conf',
-  onlyif  => '/bin/grep -q -e "worker_processes auto;" -e "worker_connections 1024;" -e "keepalive_timeout 65;" /etc/nginx/nginx.conf',
-  notify  => Exec['reload-nginx'],
+# Puppet manifest to optimize Ngnix configuration
+
+class nginx_config {
+    file { '/etc/nginx/nginx.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('nginx/nginx.conf.erb'),
+        notify  => Service['nginx'],
+    }
 }
 
-exec { 'reload-nginx':
-  command     => '/usr/sbin/service nginx reload',
-  refreshonly => true,
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Class['nginx_config'],
 }
-
